2b62acbe341cfdeb208fc2519e51de53b62fdd4d
Omkar Jadhav <jadhavomkar85@gmail.com>
2024-06-22 17:37:26 +0530

    [feat] added get routes for expenses

---
+++ data/expenses.js
@@ -0,0 +1,52 @@
+export const expenses = [
+  {
+    _id: '1',
+    title: 'Grocery Shopping',
+    amount: 250,
+  },
+  {
+    _id: '2',
+    title: 'Electricity Bill',
+    amount: 150,
+  },
+  {
+    _id: '3',
+    title: 'Internet Bill',
+    amount: 60,
+  },
+  {
+    _id: '4',
+    title: 'Gasoline',
+    amount: 40,
+  },
+  {
+    _id: '5',
+    title: 'Lunch at Cafe',
+    amount: 30,
+  },
+  {
+    _id: '6',
+    title: 'Public Transport',
+    amount: 20,
+  },
+  {
+    _id: '7',
+    title: 'Movie Tickets',
+    amount: 25,
+  },
+  {
+    _id: '8',
+    title: 'Gym Membership',
+    amount: 45,
+  },
+  {
+    _id: '9',
+    title: 'Laundry',
+    amount: 15,
+  },
+  {
+    _id: '10',
+    title: 'Coffee',
+    amount: 5,
+  },
+];
--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 import express from 'express';
+import 'dotenv/config';
+import { expenses } from './data/expenses.js';
 
 const app = express();
 
-const PORT = 5000;
+const PORT = process.env.PORT || 8000;
+
+app.get('/api/expenses', (req, res) => {
+  res.json(expenses);
+});
+
+app.get('/api/expenses/:id', (req, res) => {
+  const expense = expenses.find((obj) => obj._id === req.params.id);
+  res.json(expense);
+});
 
 app.listen(PORT, () => console.log('Server started on port: ', PORT));
