82cc1064358f95dcb4e7fa88cbdbe9b43b8ba0ce
Omkar Jadhav <jadhavomkar85@gmail.com>
2024-06-24 23:32:03 +0530

    [feat] added mongoose and cors

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 import express from 'express';
 import 'dotenv/config';
 import { expenses } from './data/expenses.js';
+import cors from 'cors';
+import connectDB from './config/db.js';
+
+await connectDB();
 
 const app = express();
 
 const PORT = process.env.PORT || 8000;
+
+app.use(cors());
 
 app.get('/api/expenses', (req, res) => {
   res.json(expenses);
