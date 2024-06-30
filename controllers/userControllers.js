3e26c9bf36ac79f7f39410172b47edb112fad2e8
Omkar Jadhav <jadhavomkar85@gmail.com>
2024-06-30 17:11:03 +0530

    [feat] added login feature

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -37,3 +37,35 @@ export const signupController = async (req, res, next) => {
     throw new Error('Error creating user');
   }
 };
+
+export const loginController = async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    res.status(400);
+    throw new Error('Incomplete data provided');
+  }
+
+  //   check if a user with same email exists or not already
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    res.status(404);
+    throw new Error('User not found!');
+  }
+
+  const isPasswordCorrect = await user.matchPassword(password);
+
+  if (isPasswordCorrect) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      pic: user.pic,
+    });
+  } else {
+    res.status(401);
+    throw new Error('Incorrect password');
+  }
+};
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,14 @@
 import express from 'express';
-import { signupController } from '../controllers/userControllers.js';
+import {
+  loginController,
+  signupController,
+} from '../controllers/userControllers.js';
 import expressAsyncHandler from 'express-async-handler';
 
 const router = express.Router();
 
 router.post('/signup', expressAsyncHandler(signupController));
+
+router.post('/login', expressAsyncHandler(loginController));
 
 export default router;
