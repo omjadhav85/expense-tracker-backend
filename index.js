4c1b1adda38438b197a65a7c5f820f6c88abff94
Omkar Jadhav <48726860+omjadhav85@users.noreply.github.com>
2024-07-27 15:40:19 +0530

    [feat] added test endpoint

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-import express from 'express';
-import 'dotenv/config';
-import { expenses } from './data/expenses.js';
-import cors from 'cors';
-import connectDB from './config/db.js';
-import userRoutes from './routes/userRoutes.js';
-import expenseRoutes from './routes/expenseRoutes.js';
-import { errorHandler, notFound } from './middlewares/errorMiddlewares.js';
-import { isAuthenticated } from './middlewares/isAuthenticated.js';
+import express from "express";
+import "dotenv/config";
+import { expenses } from "./data/expenses.js";
+import cors from "cors";
+import connectDB from "./config/db.js";
+import userRoutes from "./routes/userRoutes.js";
+import expenseRoutes from "./routes/expenseRoutes.js";
+import { errorHandler, notFound } from "./middlewares/errorMiddlewares.js";
+import { isAuthenticated } from "./middlewares/isAuthenticated.js";
 
 await connectDB();
 
@@ -17,12 +17,16 @@ const PORT = process.env.PORT || 8000;
 app.use(express.json());
 app.use(cors());
 
-app.use('/api/users', userRoutes);
+app.use("/api/users", userRoutes);
 
-app.use('/api/expenses', isAuthenticated, expenseRoutes);
+app.use("/api/expenses", isAuthenticated, expenseRoutes);
+
+app.get("/", (req, res) => {
+  res.json({ msg: "Welcome to expense tracker api service" });
+});
 
 // Error Handling middlewares
 app.use(notFound);
 app.use(errorHandler);
 
-app.listen(PORT, () => console.log('Server started on port: ', PORT));
+app.listen(PORT, () => console.log("Server started on port: ", PORT));
